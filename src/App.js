aa4b293e1acb64e342a3936f31c04e2650b3e2bb
hafizbali315 <hafizbali315@gmail.com>
2022-02-10 22:16:59 +0500

    See Gallery Section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar'
 import CreateAccountSection from './components/CreateAccountSection'
+import SeeGallerySection from './components/SeeGallerySection'
 
 function App() {
 	return (
 		<div className="App">
 			<Navbar />
 			<CreateAccountSection /> {/* Section 1 */}
+			<SeeGallerySection /> {/* Section 2 */}
 		</div>
 	)
 }
---
+++ src/components/SeeGallerySection.js
@@ -0,0 +1,43 @@
+import Image2 from '../assets/Image2.png'
+import { BsCircleFill } from 'react-icons/bs'
+
+const SeeGallerySection = () => {
+	return (
+		<div className="gallery_section">
+			<div className="left">
+				<div className="img_container">
+					<img src={Image2} alt="Image2" />
+				</div>
+			</div>
+			<div className="right">
+				<h2>Cada estrella tiene su estilo.</h2>
+				<p>
+					Elige un tema predefinido o personaliza tu
+					<br />
+					página como más te guste.
+				</p>
+
+				<div className="points_row">
+					<ul className="left">
+						<li>
+							<BsCircleFill className="icon" /> <span>Sube tu imagen de fondo.</span>
+						</li>
+						<li>
+							<BsCircleFill className="icon" /> <span>Elige el estilo de tu presentación.</span>
+						</li>
+						<li>
+							<BsCircleFill className="icon" /> <span>Personaliza tus botones.</span>
+						</li>
+					</ul>
+
+					<div className="right">
+						<p>¿Necesitas inspiración?</p>
+						<button>Ver galería</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default SeeGallerySection
