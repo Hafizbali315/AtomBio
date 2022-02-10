d59a01482417974843a964dff182d1cb83f4654f
hafizbali315 <hafizbali315@gmail.com>
2022-02-11 00:01:57 +0500

    Increase Visits Section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar'
 import CreateAccountSection from './components/CreateAccountSection'
 import SeeGallerySection from './components/SeeGallerySection'
+import IncreaseVisitsSection from './components/IncreaseVisitsSection'
 
 function App() {
 	return (
@@ -8,6 +9,7 @@ function App() {
 			<Navbar />
 			<CreateAccountSection /> {/* Section 1 */}
 			<SeeGallerySection /> {/* Section 2 */}
+			<IncreaseVisitsSection /> {/* Section 3 */}
 		</div>
 	)
 }
---
+++ src/components/IncreaseVisitsSection.js
@@ -0,0 +1,79 @@
+import DesktopSectionBackground from '../assets/DesktopSectionBackground.png'
+import Image3 from '../assets/Image3.png'
+import SpotifyImg from '../assets/SpotifyImg.png'
+import AmazonImg from '../assets/AmazonImg.png'
+import Twitch from '../assets/Twitch.png'
+import Recipe from '../assets/Recipe.png'
+import TiktokImg from '../assets/TiktokImg.png'
+
+const IncreaseVisitsSection = () => {
+	return (
+		<div className="increase_visits_section">
+			<div className="left">
+				<h2>Aumenta tus visitas y brilla más que nadie.</h2>
+				<p>
+					Guía a tus seguidores hacia tu información de contacto y tus redes.
+					<br /> Cuéntales lo que quieras sobre ti y expande tu universo.
+				</p>
+			</div>
+			<div className="right">
+				<div className="background_container">
+					<img src={Image3} alt="img3" />
+				</div>
+
+				<div className="links">
+					<div className="left">
+						<div className="last_single">
+							<button>
+								<div className="img_container">
+									<img src={SpotifyImg} alt="SpotifyImg" />
+								</div>
+								<span>Last Single</span>
+							</button>
+						</div>
+						<div className="amazon">
+							<button>
+								<div className="img_container">
+									<img src={AmazonImg} alt="SpotifyImg" />
+								</div>
+								<span>Amazon</span>
+							</button>
+						</div>
+						<div className="twitch">
+							<button>
+								<div className="img_container">
+									<img src={Twitch} alt="SpotifyImg" />
+								</div>
+								<span>Twitch</span>
+							</button>
+						</div>
+					</div>
+
+					<div className="right">
+						<div className="recipes">
+							<button>
+								<div className="img_container">
+									<img src={Recipe} alt="SpotifyImg" />
+								</div>
+								<span>Recipes</span>
+							</button>
+						</div>
+						<div className="tiktok">
+							<button>
+								<div className="img_container">
+									<img src={TiktokImg} alt="SpotifyImg" />
+								</div>
+								<span>Tiktok</span>
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="background_img">
+				<img src={DesktopSectionBackground} alt="DesktopSectionBackground" />
+			</div>
+		</div>
+	)
+}
+
+export default IncreaseVisitsSection
